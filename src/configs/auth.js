cdcbdb8f769cdc8f6baebc2f15c79b82d5388df9
Leonardo Debattisti <leonardobdebattisti@gmail.com>
2025-09-28 21:15:42 -0300

    Delete src directory

--- src/configs/auth.js
+++
@@ -1,9 +0,0 @@
-// auth --- arq. que guarda as configurações de autenticação da aplicação
-
-module.exports = {
-
-  jwt: {
-    secret: process.env.AUTH_SECRET || "default",
-    expiresIn: "1d"
-  }
-}
